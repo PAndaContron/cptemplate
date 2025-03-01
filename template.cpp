6cd9e9d21aca58be21cf4bd5e9fa341a71bebeb2
Rajat Patel <rajtel99@gmail.com>
2025-03-01 16:18:48 -0500

    vectorized types

--- template.cpp
+++ template.cpp
@@ -8,35 +8,6 @@
 
 using namespace std;
 
-#ifndef ONLINE_JUDGE
-//#define ONLINE_JUDGE
-#endif
-
-#ifndef ONLINE_JUDGE
-INLINE void __print(bool x) {cerr << (x ? "true" : "false");}
-INLINE void __print(int x) {cerr << x;}
-INLINE void __print(long x) {cerr << x;}
-INLINE void __print(long long x) {cerr << x;}
-INLINE void __print(unsigned x) {cerr << x;}
-INLINE void __print(unsigned long x) {cerr << x;}
-INLINE void __print(unsigned long long x) {cerr << x;}
-INLINE void __print(float x) {cerr << x;}
-INLINE void __print(double x) {cerr << x;}
-INLINE void __print(long double x) {cerr << x;}
-INLINE void __print(char x) {cerr << '\'' << x << '\'';}
-INLINE void __print(const char *x) {cerr << '"' << x << '"';}
-INLINE void __print(const string &x) {cerr << '"' << x << '"';}
-template<typename T, typename V> INLINE void __print(const pair<T, V> &x);
-template<typename T> INLINE void __print(const T &x);
-template<typename T, typename V> INLINE void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
-template<typename T> INLINE void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
-INLINE void _print() {cerr << "]\n";}
-template <typename T, typename... V> INLINE void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-#define D(x...) cerr << "[" << #x << "] = ["; _print(x);
-#else
-#define D(x...)
-#endif
-
 using i8 = int8_t;
 using i16 = int16_t;
 using i32 = int32_t;
@@ -60,6 +31,7 @@ using ull = unsigned long long;
 
 using f32 = float;
 using f64 = double;
+using dbl = double;
 using f80 = long double;
 using ld = long double;
 
@@ -82,6 +54,63 @@ using maxpqll = maxpq<ll>;
 using maxpqld = maxpq<ld>;
 using minpqll = minpq<ll>;
 using minpqld = minpq<ld>;
+
+#define VDEC(ty, vsz) using ty ## x ## vsz = ty __attribute__((vector_size(vsz * sizeof(ty))));
+
+VDEC(i8, 16) VDEC(i16, 8) VDEC(i32, 4) VDEC(i64, 2);
+VDEC(u8, 16) VDEC(u16, 8) VDEC(u32, 4) VDEC(u64, 2);
+VDEC(f32, 4) VDEC(f64, 2);
+
+VDEC(i8, 32) VDEC(i16, 16) VDEC(i32, 8) VDEC(i64, 4) VDEC(i128, 2);
+VDEC(u8, 32) VDEC(u16, 16) VDEC(u32, 8) VDEC(u64, 4) VDEC(u128, 2);
+VDEC(f32, 8) VDEC(f64, 4);
+
+#ifndef ONLINE_JUDGE
+//#define ONLINE_JUDGE
+#endif
+
+#ifndef ONLINE_JUDGE
+INLINE void __print(bool x) {cerr << (x ? "true" : "false");}
+INLINE void __print(int x) {cerr << x;}
+INLINE void __print(long x) {cerr << x;}
+INLINE void __print(long long x) {cerr << x;}
+INLINE void __print(unsigned x) {cerr << x;}
+INLINE void __print(unsigned long x) {cerr << x;}
+INLINE void __print(unsigned long long x) {cerr << x;}
+INLINE void __print(i128 x) {cerr << format("{}", x);}
+INLINE void __print(u128 x) {cerr << format("{}", x);}
+INLINE void __print(float x) {cerr << x;}
+INLINE void __print(double x) {cerr << x;}
+INLINE void __print(long double x) {cerr << x;}
+INLINE void __print(char x) {cerr << '\'' << x << '\'';}
+INLINE void __print(const char *x) {cerr << '"' << x << '"';}
+INLINE void __print(const string &x) {cerr << '"' << x << '"';}
+template<typename T, int N> INLINE void __printv(const T x __attribute__((vector_size(N)))) {
+    cerr << "{";
+    __print(x[0]);
+    for (int i = 1; i < int(N / sizeof(T)); i++) {
+        cerr << ", ";
+        __print(x[i]);
+    }
+    cerr << "}";
+}
+#define VPRNT(ty) INLINE void __print(const ty x) { __printv<typeof(x[0]), sizeof(ty)>(x); }
+VPRNT(i8x16) VPRNT(i16x8) VPRNT(i32x4) VPRNT(i64x2);
+VPRNT(u8x16) VPRNT(u16x8) VPRNT(u32x4) VPRNT(u64x2);
+VPRNT(f32x4) VPRNT(f64x2);
+VPRNT(i8x32) VPRNT(i16x16) VPRNT(i32x8) VPRNT(i64x4) VPRNT(i128x2);
+VPRNT(u8x32) VPRNT(u16x16) VPRNT(u32x8) VPRNT(u64x4) VPRNT(u128x2);
+VPRNT(f32x8) VPRNT(f64x4);
+template<typename T, typename V> INLINE void __print(const pair<T, V> &x);
+template<typename T> INLINE void __print(const T &x);
+template<typename T, typename V> INLINE void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T> INLINE void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+INLINE void _print() {cerr << "]\n";}
+template <typename T, typename... V> INLINE void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#define D(x...) cerr << "[" << #x << "] = ["; _print(x);
+#else
+#define D(x...)
+#endif
 
 #define lf "\n"
 #define sp " "
